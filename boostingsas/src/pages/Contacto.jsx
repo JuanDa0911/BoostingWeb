0e65bc1c2a2dd5b2642647a898f7a2e53aad4dae
JuanDavid <juandavidzhenao@gmail.com>
2025-04-08 23:57:02 -0500

    comienzo pagina web

---
+++ boostingsas/src/pages/Contacto.jsx
@@ -0,0 +1,9 @@
+export default function Contacto() {
+    return (
+      <div className="container py-5">
+        <h1 className="h3 mb-3">Contáctanos</h1>
+        <p>Envíanos un mensaje o visítanos en nuestra sede.</p>
+      </div>
+    );
+  }
+  
---
+++ boostingsas/src/pages/servicios/Generador.jsx
@@ -0,0 +1,8 @@
+export default function Generador() {
+  return (
+    <div className="container py-5">
+      <h1 className="h3 mb-3">Servicio: Generador</h1>
+      <p>Información sobre instalación, mantenimiento y venta de generadores eléctricos.</p>
+    </div>
+  );
+}
